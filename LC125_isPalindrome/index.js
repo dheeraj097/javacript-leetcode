741b3ab2bd50521045d94d0e8cd9bf8e9336d31d
dheeraj097 <dheerajdagar94@gmail.com>
2022-05-02 00:09:32 +0530

    solved LC125

--- LC125_isPalindrome/index.js
+++ LC125_isPalindrome/index.js
@@ -1,3 +1,21 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+    //santiize the string
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+    // ref: https://stackoverflow.com/questions/388996/regex-for-javascript-to-allow-only-alphanumeric
+
+    // set left to start of string and right to end of string
+    let left = 0;
+    let right = s.length - 1;
+
+    // loop
+    while(left < right){
+        if(s[left] !== s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
 
 module.exports = isPalindrome;
