603fc617e4a5589172dc99115d2892b5070707e2
dheeraj097 <dheerajdagar94@gmail.com>
2022-05-08 15:10:12 +0530

    LC5 solved

--- LC5_longestPalindrome/index.js
+++ LC5_longestPalindrome/index.js
@@ -4,6 +4,27 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    let start = 0;
+    let maxLenght = 1;
+
+    function expandAroundMiddle(left, right){
+        while(left >= 0 && right < s.length && s[left] === s[right]){
+            const currentPalLength = right-left+1;
+            if(currentPalLength > maxLenght){
+                maxLenght = currentPalLength;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        expandAroundMiddle(i-1, i+1);
+        expandAroundMiddle(i, i+1);
+    }
+
+    return s.slice(start, start + maxLenght);
+}
 
 module.exports = longestPalindrome;
