4acc47e9ac0ba706df580e3a1599e4ebe521e9b7
dheeraj097 <dheerajdagar94@gmail.com>
2022-05-22 21:32:53 +0530

    solved LC242

--- LC242_isAnagram/index.js
+++ LC242_isAnagram/index.js
@@ -1,3 +1,25 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+    if(s.length !== t.length){
+        return false;
+    }
+
+    let sCharCount = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const element = s[i];
+        sCharCount[element] = sCharCount[element] + 1 || 1;
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        const element = t[i];
+
+        if(!sCharCount[element]){
+            return false;
+        }else{
+            sCharCount[element]--;
+        }
+    }
+    return true;
+}
 
 module.exports = isAnagram;
