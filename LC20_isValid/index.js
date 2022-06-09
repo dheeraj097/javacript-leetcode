90e4b486797383f1bb4f77b3ea0a65c4d869eb8d
dheeraj097 <dheerajdagar94@gmail.com>
2022-06-09 23:11:28 +0530

    LC20 && LC49 solved

--- LC20_isValid/index.js
+++ LC20_isValid/index.js
@@ -1,3 +1,21 @@
-const isValid = s => {};
+const isValid = s => {
+    let stack = [];
+    let parenthesesObj = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]",
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const element = s[i];
+
+        if(parenthesesObj[element]){
+            stack.push(element)
+        }else if(parenthesesObj[stack.pop()] !== element){
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
 
 module.exports = isValid;
--- LC49_groupAnagrams/index.js
+++ LC49_groupAnagrams/index.js
@@ -1,3 +1,16 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+    let grouped = {};
+    for (let i = 0; i < strs.length; i++) {
+        const element = strs[i];
+        const key = element.split("").sort().join("");
+
+        if(!grouped[key]){
+            grouped[key] = [];
+        }
+
+        grouped[key].push(element);
+    }
+    return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
